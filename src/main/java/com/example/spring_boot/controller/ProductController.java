66edc8a51ceb36d5335cb915460058edee48f35f
0rque <marisergiv@gmail.com>
2021-04-25 23:47:01 +0300

    small fix

--- src/main/java/com/example/spring_boot/controller/ProductController.java
+++ src/main/java/com/example/spring_boot/controller/ProductController.java
@@ -2,20 +2,18 @@ package com.example.spring_boot.controller;
 
 import com.example.spring_boot.model.entity.Product;
 import com.example.spring_boot.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class ProductController {
 
     private final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
 
     @GetMapping("/")
     public String index() {
