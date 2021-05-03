9ffedf8f88e86dadd8a70712134d891305ad26fc
0rque <marisergiv@gmail.com>
2021-05-03 16:13:45 +0300

    annotations was added

--- src/main/java/com/example/spring_boot/service/ProductServiceImpl.java
+++ src/main/java/com/example/spring_boot/service/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.spring_boot.model.entity.Product;
 import com.example.spring_boot.model.dao.ProductDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,37 +17,44 @@ public class ProductServiceImpl implements ProductService {
 
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Product> findById(Long id) {
         return productDAO.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Product> findAll() {
         return productDAO.findAll();
     }
 
     @Override
+    @Transactional
     public Object deleteById(Long id) {
         productDAO.deleteById(id);
         return null;
     }
 
     @Override
+    @Transactional
     public Product save(Product product) {
         return productDAO.save(product);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Product> findProductsByPriceIsGreaterThan(int minPrice) {
         return productDAO.findProductsByPriceIsGreaterThan(minPrice);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Product> findProductsByPriceIsLessThan(int maxPrice) {
         return productDAO.findProductsByPriceIsLessThan(maxPrice);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Product> findProductsByPriceIsBetween(int minPrice, int maxPrice) {
         return productDAO.findProductsByPriceIsBetween(minPrice, maxPrice);
     }
