a713415a5923cd5ed3945acd5a372616d009c384
Mariia <marisergiv@gmail.com>
2021-05-23 15:08:52 +0300

    small fix

--- src/main/java/com/example/spring_boot/controller/AuthController.java
+++ src/main/java/com/example/spring_boot/controller/AuthController.java
@@ -1,11 +1,9 @@
 package com.example.spring_boot.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequiredArgsConstructor
 @Controller
 @RequestMapping
 public class AuthController {
